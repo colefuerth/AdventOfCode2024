469a06a91e9f903310669ae5ca7fed108366f803
Cole Fuerth <cfuerth@satcomdirect.com>
2024-12-06 11:59:05 -0500

    day04

--- day04/AoC.py
+++ day04/AoC.py
@@ -4,32 +4,55 @@ import sys
 from itertools import product, count, permutations
 from copy import deepcopy
 import re
-from typing import List
+from typing import List, Tuple, Dict
 
-def get_diagonals(f: List[str]) -> List[str]:
-    ret = []
-    for l in range(len(f)):
-        sublist = []
-        r = min(len(f[0]), len(f) - l)
-        for x in range(0, r):
-            sublist.append(f[l+x][x])
-        ret.append(''.join(sublist))
-    for l in range(1, len(f[0])):
-        sublist = []
-        r = min(len(f), len(f[0]) - l)
-        for x in range(0, r):
-            sublist.append(f[x][l + x])
-        ret.append(''.join(sublist))
-    return ret
+
+W = (0, -1)    # left
+E = (0, 1)     # right
+N = (-1, 0)    # up
+S = (1, 0)     # down
+NW = (-1, -1)  # top left
+NE = (-1, 1)   # top right
+SW = (1, -1)   # bottom left
+SE = (1, 1)     # bottom right
+DIRECTIONS = (N, E, S, W, NE, NW, SE, SW)
+
+
+def add(a: Tuple[int], b: Tuple[int]) -> Tuple[int]:
+    return tuple(x + y for x, y in zip(a, b))
+
+
+def is_valid_coord(coord: Tuple[int], ws: Tuple[str]):
+    return 0 <= coord[0] < len(ws) and 0 <= coord[1] < len(ws[0])
+
+
+def find_xmas_from(pos: Tuple[int, int], char: str, ws: Tuple[str], dir: Tuple[int, int] = None) -> int:
+    if char == '':
+        return 1
+    if not is_valid_coord(pos, ws) or ws[pos[0]][pos[1]] != char[0]:
+        return 0
+    if dir is None:
+        ret = 0
+        for d in DIRECTIONS:
+            ret += find_xmas_from(add(pos, d), char[1:], ws, d)
+        return ret
+    return find_xmas_from(add(pos, dir), char[1:], ws, dir)
+
 
 def part1(f: List[str]) -> int:
-    f += list(map(''.join, zip(*f)))
-    f += get_diagonals(f)
-    f += get_diagonals([list(reversed(l)) for l in f])
-    return len(re.findall(r"XMAS|SAMX", " ".join(f)))
+    return sum(
+        find_xmas_from((x, y), 'XMAS', f)
+        for x, y in product(range(len(f)), range(len(f[0])))
+    )
+
 
 def part2(f: List[str]) -> int:
-    pass
+    ret = 0
+    for c in product(range(1, len(f) - 1), range(1, len(f[0]) - 1)):
+        if f[c[0]][c[1]] == 'A':
+            corners = [[f[c[0] + NE[0]][c[1] + NE[1]], f[c[0] + SW[0]][c[1] + SW[1]]], [f[c[0] + NW[0]][c[1] + NW[1]], f[c[0] + SE[0]][c[1] + SE[1]]]]
+            ret += all(pair.count('S') == 1 and pair.count('M') == 1 for pair in corners)
+    return ret
 
 
 if __name__ == '__main__':
