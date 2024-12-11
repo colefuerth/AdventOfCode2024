5ccf5a5344f561125fd3d1183b85a49d17579039
Cole Fuerth <colefuerth@gmail.com>
2024-12-11 01:03:06 -0500

    day11 part 1

---
+++ day11/AoC.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+from copy import deepcopy
+from typing import List
+
+
+def part1(f: List[str]) -> int:
+    for _ in range(25):
+        nf = []
+        for rock in f:
+            if rock == '0':
+                nf.append('1')
+            elif len(rock) % 2 == 0:
+                nf.append(rock[:len(rock) // 2])
+                nf.append(str(int(rock[len(rock) // 2:])))
+            else:
+                nf.append(str(int(rock) * 2024))
+        f = nf
+    return len(f)
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+    f = open(fname, 'r').readlines()[0].strip().split()
+
+    print('Part 1:', part1(deepcopy(f)))
+    print('Part 2:', part2(deepcopy(f)))
