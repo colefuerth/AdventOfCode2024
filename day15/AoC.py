0861548fb3aaf0d4b75a7adb808c59db057b1853
Cole Fuerth <cfuerth@satcomdirect.com>
2024-12-17 11:02:59 -0500

    day15

--- day15/AoC.py
+++ day15/AoC.py
@@ -12,11 +12,9 @@ S = (1, 0)
 W = (0, -1)
 direction_to_coord = {'^': N, '>': E, 'v': S, '<': W}
 
+
 def add(a: Tuple[int], b: Tuple[int]) -> Tuple[int]:
     return tuple(x + y for x, y in zip(a, b))
-
-def is_valid_coord(coord: Tuple[int], f: Tuple[str]):
-    return 0 <= coord[0] < len(f) and 0 <= coord[1] < len(f[0])
 
 
 def part1(room: List[List[str]], moves: List[str]) -> int:
@@ -35,13 +33,43 @@ def part1(room: List[List[str]], moves: List[str]) -> int:
             room[x][y], room[j][k] = room[j][k], room[x][y]
             return npos
         return pos
+
     for move in moves:
         robot = apply_move(robot, move)
+
     return sum(100 * x + y for x, y in product(range(len(room)), range(len(room[0]))) if room[x][y] == 'O')
 
+
 def part2(room: List[List[str]], moves: List[str]) -> int:
-    moves = [direction_to_coord[move] for move in ''.join(moves)]
+    new_room_map = {'#':'##', 'O':'[]', '.':'..', '@':'@.'}
+    room = [list(''.join([new_room_map[pos] for pos in line])) for line in room]
     robot = next((x, y) for x, y in product(range(len(room)), range(len(room[0]))) if room[x][y] == '@')
+
+    def check_move_vertical(pos: Tuple[int, int], direction: Tuple[int, int]) -> bool:
+        x, y = pos
+        if room[x][y] == '.':
+            return True
+        if room[x][y] == '#':
+            return False
+        lbox, rbox = (pos, add(pos, E)) if room[x][y] == '[' else (add(pos, W), pos)
+        lbox2, rbox2 = add(lbox, direction), add(rbox, direction)
+        if room[lbox2[0]][lbox2[1]] == '[':
+            return check_move_vertical(lbox2, direction)
+        return check_move_vertical(lbox2, direction) and check_move_vertical(rbox2, direction)
+
+    def apply_move_vertical(pos: Tuple[int, int], direction: Tuple[int, int]) -> None:
+        x, y = pos
+        if room[x][y] == '.':
+            return
+        lbox, rbox = (pos, add(pos, E)) if room[x][y] == '[' else (add(pos, W), pos)
+        lbox2, rbox2 = add(lbox, direction), add(rbox, direction)
+        if room[lbox2[0]][lbox2[1]] == '[':
+            apply_move_vertical(lbox2, direction)
+        else:
+            apply_move_vertical(lbox2, direction)
+            apply_move_vertical(rbox2, direction)
+        room[lbox[0]][lbox[1]], room[lbox2[0]][lbox2[1]] = room[lbox2[0]][lbox2[1]], room[lbox[0]][lbox[1]]
+        room[rbox[0]][rbox[1]], room[rbox2[0]][rbox2[1]] = room[rbox2[0]][rbox2[1]], room[rbox[0]][rbox[1]]
 
     def apply_move(pos: Tuple[int, int], direction: Tuple[int, int]) -> Tuple[int, int]:
         x, y = pos
@@ -54,15 +82,15 @@ def part2(room: List[List[str]], moves: List[str]) -> int:
                 return pos
             room[x][y], room[j][k] = room[j][k], room[x][y]
             return npos
-        if room[x][y] in '[]':
-            lbox, rbox = (pos, add(pos, E)) if room[x][y] == '[' else (add(pos, W), pos)
-            lbox2, rbox2 = add(lbox, direction), add(rbox, direction)
-            if apply_move(lbox2, direction) == lbox2 or apply_move(rbox2, direction) == rbox2:
-                # TODO: this needs to be broken out into a check_move and an apply_move so recursive checks can be run before applying a move
+        if room[x][y] in '[]' and check_move_vertical(pos, direction):
+            apply_move_vertical(pos, direction)
+            return npos
         return pos
-    for move in moves:
-        robot = apply_move(robot, move)
-    return sum(100 * x + y for x, y in product(range(len(room)), range(len(room[0]))) if room[x][y] == 'O')
+
+    for move in ''.join(moves):
+        robot = apply_move(robot, direction_to_coord[move])
+
+    return sum(100 * x + y for x, y in product(range(len(room)), range(len(room[0]))) if room[x][y] == '[')
 
 
 if __name__ == '__main__':
