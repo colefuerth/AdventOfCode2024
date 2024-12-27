6841a78c06805d6ed4f8f3c3b19edf18798975a0
Cole Fuerth <colefuerth@gmail.com>
2024-12-26 19:16:03 -0500

    update template

--- template/AoC.py
+++ template/AoC.py
@@ -2,8 +2,8 @@
 
 import sys
 import pathlib
-from copy import deepcopy
 from typing import List
+from copy import deepcopy
 
 
 def part1(f: List[str]) -> int:
@@ -17,7 +17,7 @@ def part2(f: List[str]) -> int:
 if __name__ == "__main__":
     fname = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
     fname = str(pathlib.Path(__file__).parent.resolve()) + f"/{fname}"
-    f = [l.strip() for l in open(fname, "r").readlines()]
+    f = [l.strip() for l in open(fname, "r").readlines() if l.strip()]
 
     print("Part 1:", part1(deepcopy(f)))
     print("Part 2:", part2(deepcopy(f)))
