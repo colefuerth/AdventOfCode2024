357bd25aabaff5da7b1ae350bf2ca29fcf62ec6f
Cole Fuerth <colefuerth@gmail.com>
2025-01-01 12:34:15 -0500

    day24 part 2 progress, needs work

--- day24/AoC.py
+++ day24/AoC.py
@@ -4,14 +4,26 @@ import sys
 import pathlib
 from copy import deepcopy
 from typing import List, Dict
-from itertools import groupby
+from itertools import groupby, pairwise, combinations
 
 
-def solve(register: str, known: Dict[str, int], gates: Dict[str, List[str]]) -> int:
+def solve(
+    register: str,
+    known: Dict[str, int],
+    gates: Dict[str, List[str]],
+    dependency_tree: List[str] = None,
+) -> int:
     if register in known:
         return known[register]
+    if dependency_tree is None:
+        dependency_tree = []
+    if register in dependency_tree:
+        raise ValueError(f"Circular dependency detected: {register}")
+    dependency_tree.append(register)
     a, operator, b = gates[register]
-    a, b = solve(a, known, gates), solve(b, known, gates)
+    a, b = solve(a, known, gates, dependency_tree), solve(
+        b, known, gates, dependency_tree
+    )
     match operator:
         case "AND":
             c = a & b
@@ -30,7 +42,39 @@ def part1(known: Dict[str, int], gates: Dict[str, List[str]]) -> int:
 
 
 def part2(known: Dict[str, int], gates: Dict[str, List[str]]) -> int:
-    pass
+    x = "".join(str(known[x]) for x in sorted(known, reverse=True) if x.startswith("x"))
+    y = "".join(str(known[x]) for x in sorted(known, reverse=True) if x.startswith("y"))
+    z = bin(int(x, 2) + int(y, 2))[2:]
+
+    def similarity_score(a: str, b: str) -> int:
+        return sum(i == j for i, j in zip(a, b))
+
+    cz = {
+        gate: solve(gate, deepcopy(known), gates)
+        for gate in gates
+        if gate.startswith("z")
+    }
+    swaps = []
+    while cz != z:
+        scores_this_round = []
+        for a, b in combinations(gates, 2):
+            gates[a], gates[b] = gates[b], gates[a]
+            try:
+                cz = {
+                    gate: solve(gate, deepcopy(known), gates)
+                    for gate in gates
+                    if gate.startswith("z")
+                }
+                cz = "".join(str(cz[zz]) for zz in sorted(cz, reverse=True))
+                scores_this_round.append([similarity_score(z, cz), (a, b)])
+            except ValueError:
+                pass
+            gates[a], gates[b] = gates[b], gates[a]
+        a, b = max(scores_this_round, key=lambda x: x[0])[1]
+        gates[a], gates[b] = gates[b], gates[a]
+        swaps.append(a)
+        swaps.append(b)
+    return ",".join(sorted(swaps))
 
 
 if __name__ == "__main__":
