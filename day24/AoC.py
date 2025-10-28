2343e63cf9e4f67342ae5cd4dba73b985d6d0f52
Cole Fuerth <colefuerth@gmail.com>
2025-10-28 09:17:00 -0700

    aoc day 24 progress, found a solution, just need to implement it

--- day24/AoC.py
+++ day24/AoC.py
@@ -3,15 +3,17 @@
 import sys
 import pathlib
 from copy import deepcopy
-from typing import List, Dict
+from typing import List, Dict, Optional
 from itertools import groupby, pairwise, combinations
-
+from heapq import heappop, heappush, heapify
+from random import getrandbits
+from collections import defaultdict
 
 def solve(
     register: str,
     known: Dict[str, int],
     gates: Dict[str, List[str]],
-    dependency_tree: List[str] = None,
+    dependency_tree: Optional[List[str]] = None,
 ) -> int:
     if register in known:
         return known[register]
@@ -24,6 +26,7 @@ def solve(
     a, b = solve(a, known, gates, dependency_tree), solve(
         b, known, gates, dependency_tree
     )
+    c = 0
     match operator:
         case "AND":
             c = a & b
@@ -36,56 +39,39 @@ def solve(
 
 
 def part1(known: Dict[str, int], gates: Dict[str, List[str]]) -> int:
-    z = {gate: solve(gate, known, gates) for gate in gates if gate.startswith("z")}
+    z = {gate: solve(gate, known, gates)
+         for gate in gates if gate.startswith("z")}
     b = "".join(str(z[zz]) for zz in sorted(z, reverse=True))
     return int(b, 2)
 
+def build_gate_string(gate: str, known: Dict[str, int], gates: Dict[str, List[str]]) -> str:
+    if gate in known:
+        return gate
+    if gate in ["AND", "OR", "XOR"]:
+        return gate
+    a, op, b = gates[gate]
+    return f"({build_gate_string(a, known, gates)} {op} {build_gate_string(b, known, gates)})"
 
-def part2(known: Dict[str, int], gates: Dict[str, List[str]]) -> int:
-    x = "".join(str(known[x]) for x in sorted(known, reverse=True) if x.startswith("x"))
-    y = "".join(str(known[x]) for x in sorted(known, reverse=True) if x.startswith("y"))
-    z = bin(int(x, 2) + int(y, 2))[2:]
-
-    def similarity_score(a: str, b: str) -> int:
-        return sum(i == j for i, j in zip(a, b))
-
-    cz = {
-        gate: solve(gate, deepcopy(known), gates)
-        for gate in gates
-        if gate.startswith("z")
-    }
-    swaps = []
-    while cz != z:
-        scores_this_round = []
-        for a, b in combinations(gates, 2):
-            gates[a], gates[b] = gates[b], gates[a]
-            try:
-                cz = {
-                    gate: solve(gate, deepcopy(known), gates)
-                    for gate in gates
-                    if gate.startswith("z")
-                }
-                cz = "".join(str(cz[zz]) for zz in sorted(cz, reverse=True))
-                scores_this_round.append([similarity_score(z, cz), (a, b)])
-            except ValueError:
-                pass
-            gates[a], gates[b] = gates[b], gates[a]
-        a, b = max(scores_this_round, key=lambda x: x[0])[1]
-        gates[a], gates[b] = gates[b], gates[a]
-        swaps.append(a)
-        swaps.append(b)
-    return ",".join(sorted(swaps))
-
+def part2(known: Dict[str, int], gates: Dict[str, List[str]]) -> str:
+    # bitsize = len([g for g in gates if g.startswith("z")])
+    # operation = lambda x, y: x & y  # AND
+    z = {gate: build_gate_string(gate, known, gates)
+         for gate in gates if gate.startswith("z")}
+    print("\n".join(f"{zz}: {z[zz]}" for zz in sorted(z, reverse=True)))
+    
+    return ""
 
 if __name__ == "__main__":
-    fname = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+    fname = sys.argv[1] if len(sys.argv) > 1 else "small.txt"
     fname = str(pathlib.Path(__file__).parent.resolve()) + f"/{fname}"
     f = [l.strip() for l in open(fname, "r").readlines()]
     groups = [
         list(group) for is_blank, group in groupby(f, lambda x: not x) if not is_blank
     ]
-    known = {key: int(value) for key, value in [l.split(": ") for l in groups[0]]}
-    gates = {out: a.split(" ") for a, out in [l.split(" -> ") for l in groups[1]]}
+    known = {key: int(value)
+             for key, value in [l.split(": ") for l in groups[0]]}
+    gates = {out: a.split(" ")
+             for a, out in [l.split(" -> ") for l in groups[1]]}
 
     print("Part 1:", part1(deepcopy(known), deepcopy(gates)))
     print("Part 2:", part2(deepcopy(known), deepcopy(gates)))
